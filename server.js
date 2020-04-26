d27df9d099ebf36e7ab7f03c0538ff9a74dd1281
Gbenga <gbengulo005@gmail.com>
2020-04-26 12:10:20 +0100

    created http request for the diff routes

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ const express = require("express");
 
 const app = express();
 
+//Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
