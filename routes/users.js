3441297e1df932218652d2da45346a8322bcd2fc
Gbenga <gbengulo005@gmail.com>
2020-04-28 23:26:37 +0100

    created models for db

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+//Connect User Model
+const User = require("../models/User");
+
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
