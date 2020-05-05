393587ea44d74bccc690e50c9d8923acee1332fb
Gbenga <gbengulo005@gmail.com>
2020-05-05 22:46:25 +0100

    created navbar for app

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,31 @@
 import React from "react";
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const Navbar = ({ icon, title }) => {
+const Navbar = ({ title, icon }) => {
   return (
     <div className="navbar bg-primary">
-      <i className={icon} /> {title}
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </div>
   );
-  Navbar.propTypes = {
-    title: propTypes.string.isRequired,
-    icon: propTypes.string,
-  };
-  Navbar.defaultProps = {
-    title: "Contact Keeper",
-    icon: "fas fa-id-card-alt",
-  };
 };
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+Navbar.defaultProps = {
+  title: "Contact Manager",
+  icon: "fas fa-id-card-alt",
+};
+
 export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className="my-1">A react based app for managing contacts for users</p>
+      <p className="bg-dark p">
+        <strong>Version</strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+export default About;
