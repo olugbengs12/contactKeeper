73b703fb6aeb0223f38e3000c28ac4ba5b8cb1f4
Gbenga <gbengulo005@gmail.com>
2020-05-06 23:54:34 +0100

    contactcontext

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,69 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  FILTER_CONTACT,
+  UPDATE_CONTACT,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contact: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "JohnDoe@gmail.com",
+        phone: "111-111-111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "John Sawyer",
+        email: "JohnSawyer@gmail.com",
+        phone: "222-222-222",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "John Stones",
+        email: "JohnStones@gmail.com",
+        phone: "333-333-333",
+        type: "professional",
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //Add Contact
+
+  //Delete Contact
+
+  //Set Current Contact
+
+  //Clear Current Contact
+
+  //Update Contacts
+
+  //Filter Contacts
+
+  //Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
