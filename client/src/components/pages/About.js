587468cdb084f78e7801801cb3b8e07e80c37826
Gbenga <gbengulo005@gmail.com>
2020-05-06 22:39:34 +0100

    made changes to about page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,7 +4,10 @@ const About = () => {
   return (
     <div>
       <h1>About this App</h1>
-      <p className="my-1">A react based app for managing contacts for users</p>
+      <p className="my-1">
+        A react based app for managing contacts for users built with love from{" "}
+        <a href="https://github.com/olugbengs12">Gbenga</a>
+      </p>
       <p className="bg-dark p">
         <strong>Version</strong> 1.0.0
       </p>
