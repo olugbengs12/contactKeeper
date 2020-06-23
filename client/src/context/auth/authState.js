814f5dd18837717d300fa0e7caeb8acc51d64760
Gbenga <gbengulo005@gmail.com>
2020-06-23 21:30:44 +0100

    login auth for existing users

--- client/src/context/auth/authState.js
+++ client/src/context/auth/authState.js
@@ -64,6 +64,27 @@ const AuthState = (props) => {
   };
 
   //Login User
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/auth", formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
 
   //Logout
 
@@ -79,6 +100,7 @@ const AuthState = (props) => {
         loading: state.loading,
         error: state.error,
         register,
+        login,
         clearErrors,
         loadUser,
       }}
