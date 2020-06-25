d17345d7e8f46c852f976bbf256a21f55a181c19
Gbenga <gbengulo005@gmail.com>
2020-06-26 00:33:38 +0100

    prepare for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 //Connect Database
 connectDB();
@@ -12,6 +13,16 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set Static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
